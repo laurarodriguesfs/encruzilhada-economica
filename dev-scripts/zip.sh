a190a74ac22a8961d41ce796c0c025545f9ace51
Laura Rodrigues <laurarodrigues98931@gmail.com>
2023-11-02 23:36:31 -0300

    Primeiro commit

--- dev-scripts/zip.sh
+++ dev-scripts/zip.sh
@@ -1,5 +1,5 @@
-#!/bin/bash 
+#!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CDIR=$( pwd )
 cd $DIR/../themes
-zip -r ../zips/base-theme-slug.zip base-theme-slug -x "base-theme-slug/node_modules/*"
+zip -r ../zips/tema-conpedi.zip tema-conpedi -x "tema-conpedi/node_modules/*"
---
+++ themes/tema-conpedi/webpack.mix.js
@@ -0,0 +1,43 @@
+const path = require( 'path' );
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for your application, as well as bundling up your JS files.
+ |
+ */
+
+const root_dir = './';
+const assets_dir = root_dir + '/assets';
+const dist_dir = root_dir + '/dist';
+
+mix.js(assets_dir + '/javascript/app.js', '');
+mix.sass(assets_dir + '/scss/app.scss', '').sourceMaps();
+
+
+mix.webpackConfig({
+	entry: {
+		//imageBlock: './assets/javascript/blocks/imageBlock/index.js',
+    },
+    
+    output: {
+        chunkFilename: dist_dir + '/[name].js',
+        path: path.resolve( __dirname, './dist/' ),
+        publicPath: dist_dir,
+        filename: '[name].js',
+    },
+
+    module: {
+		
+    },
+  
+	devtool: "inline-source-map" 
+});
+
+
+
